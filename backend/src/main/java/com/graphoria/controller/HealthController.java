995eea959abbcba86a19f5a853db886911955f32
Pavitthiran <pavitthiranrock@gmail.com>
2026-03-02 18:54:16 +0530

    Fix HealthController

--- backend/src/main/java/com/graphoria/controller/HealthController.java
+++ backend/src/main/java/com/graphoria/controller/HealthController.java
@@ -4,3 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Graphoria backend is live";
+    }
+}
